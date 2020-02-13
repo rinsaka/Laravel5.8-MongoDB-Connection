8cf4a4e4557fce947f6d5fac2f672808784d4409
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2020-02-13 09:02:33 +0900

    model

--- app/Comment.php
+++ app/Comment.php
@@ -2,9 +2,12 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Database\Eloquent\Model;  // ここをコメントアウトする
+use Jenssegers\Mongodb\Eloquent\Model as Moloquent;
 
-class Comment extends Model
+class Comment extends Moloquent
 {
-  //
+  // データベースのコレクション名を指定する
+  // （RDM のテーブル名のようなイメージ）
+  protected $collection = 'comments';
 }
